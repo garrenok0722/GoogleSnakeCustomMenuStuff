55ede910691b8048d1b1ce055c0930142ed594c6
garrenok0722 <63005987+garrenok0722@users.noreply.github.com>
2021-12-09 12:21:22 -0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,6 +8,13 @@ window.snake.more_menu = function(e) {
   img.height = 47;
   document.querySelector('#size').appendChild(img);
 
+  // test
+  img = new Image;
+  img.src = 'https://postimg.cc/jW5Wj1Vm';
+  img.width = 47;
+  img.height = 47;
+  document.querySelector('#size').appendChild(img);
+
   // tiny
   img = new Image;
   img.src = 'https://i.postimg.cc/dtr6JBNh/tiny-1.png';
